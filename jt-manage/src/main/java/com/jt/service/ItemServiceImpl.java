3463ad1ca694e41c2b97ebc0882680b06c2f9e64
hao <252927993@qq.com>
2019-07-30 20:39:29 +0800

    Initial commit

---
+++ jt-manage/src/main/java/com/jt/service/ItemServiceImpl.java
@@ -0,0 +1,15 @@
+package com.jt.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jt.mapper.ItemMapper;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+    @Autowired
+    private ItemMapper itemMapper;
+
+
+}
